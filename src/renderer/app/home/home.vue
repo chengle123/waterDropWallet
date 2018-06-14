a207945228d2e36d60eb268b24955880aeb6d18e
chengle123 <314737853@qq.com>
2018-06-14 10:23:40 +0800

    1

--- src/renderer/app/home/home.vue
+++ src/renderer/app/home/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <iframe src="http://emym.top/" width="100%" height="100%" frameborder="0"></iframe>
+    <iframe src="http://waterdrop.emym.top/" width="100%" height="100%" frameborder="0"></iframe>
 </template>
 <script>
 export default {
